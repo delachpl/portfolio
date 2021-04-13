fe5b2e6a2283fb7e531b214ce8cc3aa4ba2c2a87
delachpl <69765419+delachpl@users.noreply.github.com>
2021-04-13 15:36:58 +0200

    Initial commit

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4bfdf931995429437434e2388e3e83d4'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f540828b-679c-4a22-b0a4-f4e7fae51ddd'
+};
